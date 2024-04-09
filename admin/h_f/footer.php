cd9f7ffde658bdd9fb44fd75710edde5f9b6cda0
Ocean <77823676+Ocean48@users.noreply.github.com>
2024-04-08 21:25:06 -0400

    Added Header and Footer

---
+++ admin/h_f/footer.php
@@ -0,0 +1,9 @@
+<footer>
+        <p class="copyright">Copyright &copy;
+            <script>document.write(new Date().getFullYear())</script>StackUnderFlow All Rights Reserved
+        </p>
+    </footer>
+
+</body>
+
+</html>
---
+++ admin/h_f/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>StackUnderFlow</title>
+</head>
+
+<body>
+
+    <header>
+        <br style="margin-bottom: 10px;">
+        <a href="index.php" style="float: left;">
+            <img class="header_logo" src="" alt="Logo">
+        </a>
+        <a href="" style="float: left;">
+            <p style="color: #FFFFFF; float: left; margin-left: 5vw; font-size: 35px;">XXXXX</p>
+        </a>
+        <a href="" style="float: left;">
+            <p style="color: #FFFFFF; float: left; margin-left: 5vw; font-size: 35px;">YYYYY</p>
+        </a>
+        <a href="logout.php" style="float: left;">
+            <p style="color: #FFFFFF; float: left; margin-left: 5vw; font-size: 35px;">ZZZZZ</p>
+        </a>
+        <br style="margin-bottom: 10px; clear: both;">
+        <br style="margin-bottom: 10px; clear: both;">
+    </header>
