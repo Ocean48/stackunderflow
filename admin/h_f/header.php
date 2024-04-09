6bc300d208dae573e9002ad8585f956752b61392
Ocean <77823676+Ocean48@users.noreply.github.com>
2024-04-08 21:37:57 -0400

    Adding header style

--- admin/h_f/header.php
+++ admin/h_f/header.php
@@ -17,16 +17,16 @@
             <img style="width: 150px;" class="header_logo" src="../t.png" alt="Logo">
         </a>
         <a href="index.php" style="float: left;">
-            <p style="color: #000000; float: left; margin-left: 5vw; font-size: 35px;">Home</p>
+            <p >Home</p>
         </a>
         <a href="" style="float: left;">
-            <p style="color: #000000; float: left; margin-left: 5vw; font-size: 35px;">YYYYY</p>
+            <p >YYYYY</p>
         </a>
         <a href="login.php" style="float: left;">
-            <p style="color: #000000; float: left; margin-left: 5vw; font-size: 35px;">Login</p>
+            <p >Login</p>
         </a>
         <a href="logout.php" style="float: left;">
-            <p style="color: #000000; float: left; margin-left: 5vw; font-size: 35px;">Logout</p>
+            <p >Logout</p>
         </a>
         <br style="margin-bottom: 10px; clear: both;">
         <br style="margin-bottom: 10px; clear: both;">
