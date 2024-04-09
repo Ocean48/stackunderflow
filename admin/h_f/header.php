1fb5f5fa0cd6bb8954e8811dfa1bb26da16f86cc
oceanc48 <c2505384203@gmail.com>
2024-04-08 22:31:24 -0400

    style setup

--- admin/h_f/header.php
+++ admin/h_f/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>StackUnderFlow</title>
 </head>
 
@@ -14,7 +14,7 @@
     <header>
         <br style="margin-bottom: 10px;">
         <a href="index.php" style="float: left;">
-            <img style="width: 150px;" class="header_logo" src="../t.png" alt="Logo">
+            <img id="header_logo" src="../t.png" alt="Logo">
         </a>
         <a href="index.php" style="float: left;">
             <p >Home</p>
