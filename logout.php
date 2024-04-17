d0b8503016a7eda069752630c5d426e5385f13bf
Ocean <77823676+Ocean48@users.noreply.github.com>
2024-04-17 02:47:07 -0400

    header UI update

--- logout.php
+++ logout.php
@@ -3,6 +3,6 @@ require_once('admin/private/initialize.php');
 
 session_destroy(); //destroy the session since it was only used for admin login info
 
-header("Location: login.php")
+header("Location: index.php")
 
 ?>
