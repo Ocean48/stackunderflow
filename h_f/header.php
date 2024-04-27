ce039ef397a7c093043db4facbb71bcfa7980a95
Ocean <77823676+Ocean48@users.noreply.github.com>
2024-04-27 16:41:51 -0400

    adding more pages

--- h_f/header.php
+++ h_f/header.php
@@ -24,9 +24,9 @@
 
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                     <li><a href="index.php" class="nav-link px-2 text-secondary">Home</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">Posts</a></li>
+                    <li><a href="faqs.php" class="nav-link px-2 text-white">FAQs</a></li>
+                    <li><a href="about.php" class="nav-link px-2 text-white">About</a></li>
                 </ul>
 
                 <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
